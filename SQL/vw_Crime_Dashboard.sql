2afcb86a2cbaa838a3fdd513d9dcf767ee2cb9d6
Pinar Bozyigit <pinarbozyigit1@gmail.com>
2026-03-12 22:20:48 +0000

    Add files via upload

--- SQL/vw_Crime_Dashboard.sql
+++ SQL/vw_Crime_Dashboard.sql
@@ -1,1 +1,32 @@
+USE [London_Crime_Analysis]
+GO
+
+/****** Object:  View [dbo].[vw_Crime_Dashboard]    Script Date: 12/03/2026 21:11:52 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER view [dbo].[vw_Crime_Dashboard] as
+select 
+  case 
+    when cl.[Month] IS null then null
+    when LEN(cl.[Month]) =7
+    then cast(cl.[Month]+'-01' as date)
+else
+CAST(cl.[Month] as date)
+end 
+as Month_date
+
+      ,cl.[Crime_type]
+      ,cl.[Borough]
+      ,COUNT(*) as Crime_Count
+from 	 [London_Crime_Analysis].[dbo].[Crime_Law] cl 
+join Dim_Borough db on  cl.Borough=db.borough_name
+group by Crime_type,Month,Borough
+
+GO
+
 
--- SQL/vw_London_Crime.sql
+++ SQL/vw_London_Crime.sql
@@ -1,1 +1,40 @@
+USE [London_Crime_Analysis]
+GO
+
+/****** Object:  View [dbo].[vw_London_Crime]    Script Date: 12/03/2026 21:15:32 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+ALTER view [dbo].[vw_London_Crime] as
+select 
+  case 
+when cl.[Month] IS null then null
+when LEN(cl.[Month]) =7
+then cast(cl.[Month]+'-01' as date)
+else
+CAST(cl.[Month] as date)
+end 
+as Month_date
+
+      ,cl.[Reported_by]
+      ,cl.[Falls_within]
+      ,cl.[Longitude]
+      ,cl.[Latitude]
+      ,cl.[Location]
+      ,cl.[LSOA_code]
+      ,cl.[LSOA_name]
+      ,cl.[Crime_type]
+      ,cl.[Last_outcome_category]
+      ,cl.[Context]
+      ,cl.[Borough]
+from 	 [London_Crime_Analysis].[dbo].[Crime_Law] cl 
+join Dim_Borough db on  cl.Borough=db.borough_name
+
+GO
+
 
